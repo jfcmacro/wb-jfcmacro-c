0acf51fedf7fe15e0d79f45c2b61cca07833c539
Juan Cardona-EPAM <Juan_Cardona@epam.com>
2023-02-26 02:01:02 +0000

    Examples c

--- git/libgit2/examples/main.c
+++ git/libgit2/examples/main.c
@@ -8,8 +8,8 @@ main(int argc, char* argv[]) {
   git_libgit2_init();
   int error;
   git_repository *repo = NULL;
-  /* if ((error = lg2_clone(NULL, argc, argv)) < GIT_OK) { */
-  if ((error = lg2_simple_clone(&repo, argv[1], argv[2])) < GIT_OK) {
+  if ((error = lg2_clone(NULL, argc, argv)) < GIT_OK) {
+  /* if ((error = lg2_simple_clone(&repo, argv[1], argv[2])) < GIT_OK) { */
     const git_error *e = git_error_last();
     printf("Error %d/%d: %s\n", error, e->klass, e->message);
     return EXIT_FAILURE;
